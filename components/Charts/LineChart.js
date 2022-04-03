25ea6a836652ad552bd45eecde51ff04702b58e8
sohan patel <sohan.m.patel@leidos.com>
2022-04-03 11:49:46 -0400

    progress push

--- components/Charts/LineChart.js
+++ components/Charts/LineChart.js
@@ -16,39 +16,47 @@
 
 */
 
-import React from "react";
-import ReactApexChart from "react-apexcharts";
+import React from 'react';
+//import ReactApexChart from 'react-apexcharts';
+
+import dynamic from 'next/dynamic';
+
+const ReactApexChart = dynamic(() => import('react-apexcharts'), {
+	ssr: false,
+});
 
 class LineChart extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      chartData: [],
-      chartOptions: {},
-    };
-  }
+		this.state = {
+			chartData: [],
+			chartOptions: {},
+		};
+	}
 
-  componentDidMount() {
-    const { lineChartData, lineChartOptions } = this.props;
+	componentDidMount() {
+		const { lineChartData, lineChartOptions } = this.props;
 
-    this.setState({
-      chartData: lineChartData,
-      chartOptions: lineChartOptions,
-    });
-  }
+		this.setState({
+			chartData: lineChartData,
+			chartOptions: lineChartOptions,
+		});
+	}
 
-  render() {
-    return (
-      <ReactApexChart
-        options={this.state.chartOptions}
-        series={this.state.chartData}
-        type='area'
-        width='100%'
-        height='100%'
-      />
-    );
-  }
+	render() {
+		if (typeof window !== 'undefined') {
+			return (
+				<ReactApexChart
+					options={this.state.chartOptions}
+					series={this.state.chartData}
+					type="area"
+					width="100%"
+					height="100%"
+				/>
+			);
+		}
+	}
 }
 
 export default LineChart;
