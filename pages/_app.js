91f57796949f502d98780e146a2d8e460d83daf9
sohan patel <sohan.m.patel@leidos.com>
2022-04-03 09:30:04 -0400

    sidebar component

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,28 @@ import '../styles/globals.css';
 import { Toaster } from 'react-hot-toast';
 import { SessionProvider as AuthProvider } from 'next-auth/react';
 import { ChakraProvider } from '@chakra-ui/react';
+import { extendTheme } from '@chakra-ui/react';
+
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+	brand: {
+		900: '#1a365d',
+		800: '#153e75',
+		700: '#2a69ac',
+	},
+};
+
+const theme = extendTheme({ colors });
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
 	// Setting the session prop allows session state to be shared between pages
 	return (
 		<>
-			<AuthProvider session={session}>
-				<Component {...pageProps} />
-			</AuthProvider>
+			<ChakraProvider theme={theme}>
+				<AuthProvider session={session}>
+					<Component {...pageProps} />
+				</AuthProvider>
+			</ChakraProvider>
 			<Toaster />
 		</>
 	);
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Layout from '@/components/Layout';
 import Grid from '@/components/Grid';
+import { Box } from '@chakra-ui/react';
+import SimpleSidebar from 'components/Sidebar.tsx';
 import { PrismaClient } from '@prisma/client';
 import { getSession } from 'next-auth/react';
 
@@ -29,16 +31,11 @@ export async function getServerSideProps(context) {
 
 export default function Home(props) {
 	return (
-		<Layout>
-			<h1 className="text-xl font-medium text-gray-800">
-				Top-rated places to stay
-			</h1>
-			<p className="text-gray-500">
-				Explore some of the best places in the world
-			</p>
-			<div className="mt-8">
-				<Grid homes={props.home} />
-			</div>
-		</Layout>
+		<>
+			<SimpleSidebar />
+			<Box bg="tomato" w="100%" p={4} color="white">
+				This is the Box
+			</Box>
+		</>
 	);
 }
